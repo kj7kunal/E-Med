ac5824066af0e1ddfb722bce3b8b9e5e26f28537
kobe <kobe@vps-MacBook-Air.local>
2017-08-16 19:44:39 -0700

    physician handlebars and route

--- controllers/ps_controller.js
+++ controllers/ps_controller.js
@@ -4,6 +4,9 @@ const express = require("express"),
 router.get("/", function(req, res) {
     res.render("index");
 });
+router.get("/physician", function(req, res) {
+    res.render("physician");
+});
 
 
 module.exports = router;
