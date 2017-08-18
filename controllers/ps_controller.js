b42e573cd574b9218bc2d4331602dda542d65045
Narin Sundarabhaya <nrs710@gmail.com>
2017-08-18 00:11:33 -0700

    added get route for /patients

--- controllers/ps_controller.js
+++ controllers/ps_controller.js
@@ -1,9 +1,18 @@
 const express = require("express"),
-    router = express.Router();
+    router = express.Router(),
+    db = require('../models');
 
 router.get("/", function(req, res) {
     res.render("index");
 });
+
+router.get("/patients", function(req, res) {
+    db.patient.findAll().then((result) => {
+        console.log(result);
+        var hbsObject = { patient: result };
+        res.render("patients_list", hbsObject);
+    })
+})
 
 
 module.exports = router;
