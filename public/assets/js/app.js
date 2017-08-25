3302f2b83f63ae2e76b1f609241e19d8455acedb
hovsepa <hello@hovsep.io>
2017-08-24 18:13:05 -0700

    physician add task through modal, add specialists route

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -16,18 +16,41 @@ function deleteSpecialist(event) {
     var id = $(this).data("id");
     console.log($(this).data("id"))
     $.ajax({
-      method: "DELETE",
-      url: "/specialists/delete/" + id
-    }).done(function(result){
+        method: "DELETE",
+        url: "/specialists/delete/" + id
+    }).done(function (result) {
         console.log(result);
         window.location.href = result.url;
     });
-  }
-
-function checkClick (event) {
-    console.log($(this).data("id"));
 }
 
+function deleteTask(event) {
+    event.stopPropagation();
+    var id = $(this).data("id");
+    console.log($(this).data("id"))
+    $.ajax({
+        method: "DELETE",
+        url: "/tasks/delete/" + id
+    }).done(function (result) {
+        console.log(result);
+        window.location.href = result.url;
+    });
+}
+// deletes specialists on specialists page
 $(document).on("click", ".minus.icon", deleteSpecialist);
-$(document).on("click", "input[type='checkbox']", checkClick);
+// deletes tasks on physician page
+$(document).on("click", "input[type='checkbox']", deleteTask);
+// shows module on physician page
+$(document).on("click", "i.add.circle.icon", function(){
+    $('.ui.modal').modal('show', {
+        onApprove : function() {
+            return false;
+        }
+    });
+});
 
+function hideModal() {
+    $('.ui.modal').modal('hide');
+}
+
+$(document).on("click", "#cancel-modal", hideModal);
