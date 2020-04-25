a0a892fc09bfbdb29ad3d86fe307285e21713970
Kunal Jain <kunal.jain@bizreach.co.jp>
2020-04-25 20:25:27 +0900

    modified admin model

--- models/admin.js
+++ models/admin.js
@@ -21,14 +21,8 @@ module.exports = function(sequelize, DataTypes) {
             allowNull: false,
             defaultValue: "admin",
         }
-    }, 
-    {
-        hooks: {
-            beforeCreate: function(admin) {
-                admin.password = bcrypt.hashSync(admin.password, bcrypt.genSaltSync(10), null);
-            }
-        }
     });
+    
     // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
     Admin.prototype.validPassword = function(password) {
         return bcrypt.compareSync(password, this.password);
