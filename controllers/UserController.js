c4062f3cdd214993215428d5423d542093d9174c
pikujs <pikujs@gmail.com>
2020-05-17 02:21:45 +0530

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,90 +4,88 @@ const express = require("express"),
     isAuthenticated = require("../config/middleware/isAuthenticated"),
     passport = require("../config/passport");
 
+//const { WebhookClient } = require('dialogflow-fulfillment');
 
-class PatientsController {
-  /**
-   * Liste of Articles
-   * @param {*} req
-   * @param {*} res
-   */
-  liste(req, res) {
-    db.Articles.findAll().then(articles =>
-      res.render("articles/index", { articles })
-    );
-  }
-  /**
-   * Store a new patient in database
-   * @param {*} req
-   * @param {*} res
-   */
-  store(patientInfo) {
-  	console.log("Storing new patient in DB");
-    db.patient.create({
-            "first_name": patientInfo.firstName,
-            "last_name": patientInfo.lastName,
-            "dob": patientInfo.dob,
-            "gender": patientInfo.gender,
-            "street_address": patientInfo.streetAddress,
-            "city": patientInfo.city,
-            "gender": "F",
-            "state": patientInfo.state,
-            "zip": patientInfo.zip,
-            "telephone": patientInfo.telephone,
-            "height": patientInfo.height,
-            "weight": patientInfo.weight,
-            "allergies": patientInfo.allergies,
-            "procedures": patientInfo.procedures,
-            "emergency_contact_name": patientInfo.emergencyContactName,
-            "emergency_contact_number": patientInfo.emergencyContactNumber,
-            "provider_name": patientInfo.providerName,
-            "image": patientInfo.image
+class UserRegistrationController {
+  
+  newUserIntent(agent) {
+    console.log("Storing new User in DB");
+    let fName = agent.parameters["given-name"];
+    let cNum = agent.parameters["user-contact"];
+
+    if(db.user.findAll({where: {phone_number: cNum}}).length > 0) return "Phone Number already Exists";
+    fulfillmentText = "Hi " + fName + ", welcome to E-Medic. Please check your mail for a confirmation email from us.\
+            \nMeanwhile what would you like to do today?";
+    db.user.create({
+            "first_name": fName,
+            "last_name": agent.parameters["last-name"],
+            "email": agent.parameters["email"],
+            "phone_number": cNum
         })
         .then(() => {
-            return res.redirect("/physician/list");
+            return fulfillmentText;
+        });
+  }
+
+
+  newPatientIntent(agent) {
+  	console.log("Storing new patient in DB");
+    let fName = agent.parameters["given-name"];
+    let lName = agent.parameters["last-name"];
+    let cNum = agent.parameters["patient-contact"];
+
+    if(db.user.findAll({where: {phone_number: cNum}}).length > 0) return "Phone Number already Exists";
+    fulfillmentText = "Thanks for adding " + fName + " " + lName + ".\
+        \nYou can now proceed to book a consultation for your registered patients. What would you like to do today?\
+        \n1. Register another patient?\
+        \n2. Book New Consultation?\
+        \n3. Continue Old Consultation?\
+        \n4. List registered patients?\
+        \n5. Check patient details?\
+        \n6. Update patient details?\
+        \n7. More information (Data policies)?";
+    db.patient.create({
+            "first_name": fName,
+            "last_name": lName,
+            "dob": agent.parameters["dob"],
+            "gender": agent.parameters["gender"],
+            "telephone": cNum,
+            "height": agent.parameters["height"],
+            "weight": agent.parameters["weight"],
+            "allergies": agent.parameters["allergies"],
+            "procedures": agent.parameters["procedures"],
+            "blood_type": agent.parameters["blood-type"]
         })
+        .then(() => {
+            return fulfillmentText;
+        })
+        .catch(err => return err);
   }
 
-  /**
-   * Update a article
-   * @param {*} req
-   * @param {*} res
-   */
-  update(req, res) {
-    const id = req.params.id;
-    db.Articles.findById(id).then(article =>
-      res.render("articles/update", { article })
-    );
-  }
-  /**
-   * Upgrade a article in database
-   * @param {*} req
-   * @param {*} res
-   */
-  upgrade(req, res) {
-    const id = req.params.id;
-    db.Articles.update(req.body, { where: { id } }).then(article =>
-      res.redirect("/articles/liste")
-    );
-  }
-  /**
-   * Remove an article in database
-   * @param {*} req
-   * @param {*} res
-   */
-  remove(req, res) {
-    const id = req.params.id;
-    db.Articles.destroy({
-      where: { id }
-    }).then(() => res.redirect("/articles/liste"));
+  liste(agent) {
+    db.user.findAll().then(users => {
+      fulfillmentText = "These are the users";
+      return fulfillmentText;
+    });
   }
 
-  /**
-   * Show a article
-   * @param {*} req
-   * @param {*} res
-   */
-  show(req, res) {
+  update(agent) {
+    let cNum = agent.parameters["telephone"];
+    //if(db.user.findAll({where: {phone_number: cNum}}).length > 0) return "Phone Number already Exists";
+    db.patient.update(agent.parameters, where: {telephone: cNum})
+      .then(() => return "Patient Updated")
+      .catch(err => return err);
+  }
+  
+  remove(agent) {
+    let cNum = agent.parameters["telephone"];
+    db.patient.destroy({
+      where: { telephone: cNum }
+    }).then(() => return "Patient Deleted")
+    .catch(err => return err);
+  }
+
+  show(agent) {
     const id = req.params.id;
     db.Articles.findById(id).then(article =>
       res.render("articles/show", { article })
@@ -95,4 +93,26 @@ class PatientsController {
   }
 }
 
-module.exports = PatientsController;
+module.exports = UserRegistrationController;
+
+
+  /*function cardFunc(agent) {
+    agent.add(`This message is from Dialogflow's Cloud Functions for Firebase inline editor!`);
+    agent.add(new Card({
+        title: `Title: this is a card title`,
+        imageUrl: 'https://dialogflow.com/images/api_home_laptop.svg',
+        text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+        buttonText: 'This is a button',
+        buttonUrl: 'https://docs.dialogflow.com/'
+      });
+    );
+    agent.add(new Suggestion(`Quick Reply`));
+    agent.add(new Suggestion(`Suggestion`));
+    agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+  }
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('UserInfo', storeUserInfo);
+  intentMap.set('appointmentscheduler', schAppoint)
+  agent.handleRequest(intentMap);
+  */
