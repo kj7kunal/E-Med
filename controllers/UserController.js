76cbe22f43f75e8e019c43f44732d8e5c02dec17
pikujs <pikujs@gmail.com>
2020-05-31 10:43:55 +0530

    Update UserController.js
    Updated according to new db schema

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,19 +8,19 @@ const express = require("express"),
 
 class UserRegistrationController {
   
-  newUserIntent(agent) {
+  newUserIntent(agent, body) {
     console.log("Storing new User in DB");
     let fName = agent.parameters["user-given-name"];
-    let cNum = agent.parameters["user-contact"];
+    let cNum = body.From.toSring();
 
-    if(db.user.findAll({where: {phone_number: cNum}}).length > 0) return "Phone Number already Exists";
+    if(db.userWA.findAll({where: {phone_number: cNum}}).length > 0) return "Phone Number already Exists";
     fulfillmentText = "Hi " + fName + ", welcome to E-Medic. Please check your mail for a confirmation email from us.\
-            \nMeanwhile what would you like to do today?";
-    db.user.create({
+            \nMeanwhile please share your location in the chat. Clip Icon -> Location";
+    db.userWA.create({
             "first_name": fName,
             "last_name": agent.parameters["last-name"],
             "email": agent.parameters["email"],
-            "phone_number": cNum
+            "wa_phone_number": cNum
         })
         .then(() => {
             return fulfillmentText;
@@ -28,15 +28,23 @@ class UserRegistrationController {
         .catch(err => return err);
   }
 
+  addLocation(body){    
+    db.userWA.update({
+      loc_long: body.Logitude.toSring(),
+      loc_lat: body.Latitude.toSring()
+    }, { where: {"phone_number": body.From.toSring()}})
+    .then({
+      return "Updated Location";
+    })
+    .catch(err => return err);
+  }
 
-  newPatientIntent(agent) {
+  newPatientIntent(agent, body) {
   	console.log("Storing new patient in DB");
     let fName = agent.parameters["patient-given-name"];
     let lName = agent.parameters["patient-last-name"];
-    let cNum = agent.parameters["patient-contact"];
-    let uid = agent.parameters["user-id"];
+    let parentUser = await db.userWA.findOne(where: {phone_number: body.From.toSring()});
 
-    if(db.user.findAll({where: {phone_number: cNum}}).length > 0) return "Phone Number already Exists";
     fulfillmentText = "Thanks for adding " + fName + " " + lName + ".\
         \nYou can now proceed to book a consultation for your registered patients. What would you like to do today?\
         \n1. Register another patient?\
@@ -46,18 +54,11 @@ class UserRegistrationController {
         \n5. Check patient details?\
         \n6. Update patient details?\
         \n7. More information (Data policies)?";
-    db.patient.create({
+
+    db.patientWA.create({
             "first_name": fName,
-            "last_name": lName,
-            "dob": agent.parameters["dob"],
-            "gender": agent.parameters["gender"],
-            "telephone": cNum,
-            "height": agent.parameters["height"],
-            "weight": agent.parameters["weight"],
-            "allergies": agent.parameters["allergies"],
-            "procedures": agent.parameters["procedures"],
-            "blood_type": agent.parameters["blood-type"],
-            "user_id": uid,
+            "last_name": lName
+            "userWAid": parentUser.id,
             "is_user" : (agent.parameters["is_user"].toLowerCase() === "yes")
         })
         .then(() => {
@@ -66,9 +67,57 @@ class UserRegistrationController {
         .catch(err => return err);
   }
 
-  liste(agent) {
+  addPatientInfoIntent(agent, body){
+    let cNum = agent.parameters["patient-contact"];
+    let pUser = await db.userWA.findOne(where: {phone_number: body.From.toSring()});
+    let patient = await db.patientWA.findOne(where: {User_id: pUser.id});
+
+    fulfillmentText = "Thanks for adding " + fName + " " + lName + ".\
+        \nYou can now proceed to book a consultation for your registered patients. What would you like to do today?\
+        \n1. Register another patient?\
+        \n2. Book New Consultation?\
+        \n3. Continue Old Consultation?\
+        \n4. List registered patients?\
+        \n5. Check patient details?\
+        \n6. Update patient details?\
+        \n7. More information (Data policies)?";
+    db.patientInfoWA.create({
+            "first_name": fName,
+            "last_name": lName,
+            "dob": agent.parameters["dob"],
+            "sex": agent.parameters["gender"],
+            "telephone": cNum,
+            "height_cm": agent.parameters["height"],
+            "weight_kg": agent.parameters["weight"],
+            "blood_type": agent.parameters["blood-type"],
+            "patientWAid": patient.id,
+        })
+        .then(() => {
+            return fulfillmentText;
+        })
+        .catch(err => return err);
+  }
+
+  updateAilAllergies(agent, body){
+    let user = await db.userWA.findOne(where: {phone_number: body.From.toSring()});
+    let patient = await db.patientWA.findOne(where: {userWAid: user.id});
+    
+    let fulfillmentText = "We have updated your Allergies and Ailments. Do you want to Consult a doctor? (More Options here)"
+
+    db.patientInfoWA.update({
+      Ailments: agent.parameters["Ailments"],
+      Allergies: agent.parameters["Allergies"]
+    }, { where: {"patientWAid": patient.id}})
+    .then({
+      return fulfillmentText;
+    })
+    .catch(err => return err);
+  }
+
+  liste(agent, body) {
     let fulfillmentText = "These are your Patients: ";
-    db.patient.findAll({where: {uid: agent.parameters["user-id"]}}).then(patients => {
+    let user = await db.userWA.findOne(where: {phone_number: body.From.toSring()});
+    db.patientWA.findAll({where: {User_id: user.id}}).then(patients => {
       if (patients.length === 0) throw "You need to register a patient first. Would you like to register a new patient?";
       patients.forEach((patient, i) => {
         fulfillmentText = fulfillmentText + "\n\nPatient " + i + ": " + 
@@ -89,15 +138,15 @@ class UserRegistrationController {
 
   update(agent) {
     let cNum = agent.parameters["telephone"];
-    //if(db.user.findAll({where: {phone_number: cNum}}).length > 0) return "Phone Number already Exists";
-    db.patient.update(agent.parameters, where: {telephone: cNum})
+    //if(db.userWA.findAll({where: {phone_number: cNum}}).length > 0) return "Phone Number already Exists";
+    db.patientWA.update(agent.parameters, where: {telephone: cNum})
       .then(() => return "Patient Updated")
       .catch(err => return err);
   }
   
   remove(agent) {
     let cNum = agent.parameters["telephone"];
-    db.patient.destroy({
+    db.patientWA.destroy({
       where: { telephone: cNum }
     }).then(() => return "Patient Deleted")
     .catch(err => return err);
@@ -106,7 +155,7 @@ class UserRegistrationController {
   show(agent) {
     let uid = agent.parameters["user-id"];
     let fulfillmentText = "These are the details of your Patient: ";
-    db.patient.findAll({where: { user_id : uid, 
+    db.patientWA.findAll({where: { user_id : uid, 
                                   first_name: agent.parameters["patient-given-name"]}
                                 }).then(patients =>
                                   if(patients.length == 1){
