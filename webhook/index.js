8691c2c30bf377d4e905e6d543ab83d0dce07abe
pikujs <pikujs@gmail.com>
2020-06-16 00:26:46 +0530

    some minor changes
    last commit for today

--- webhook/index.js
+++ webhook/index.js
@@ -34,9 +34,13 @@ router.post('/api/chat/', async function(req, res) {
     contextClient.listContexts({parent: formattedParent})
         .then(responses => {
             const cNames = responses[0];
-            for (cName of cNames)
-                if (cName == "share_loc")
+            for (cName of cNames){
+                if (cName == "share_loc"){
                     responseText = userController.addLocation(body);
+                    contextClient.deleteContext({parent: formattedParent})
+                        .catch(err => responseText += ("\n" + err));
+                }
+            }
         })
         .catch(err => responseText = err);
 
@@ -46,9 +50,47 @@ router.post('/api/chat/', async function(req, res) {
     const twiml = new MessagingResponse();
 
     // INTENTS
+    // Directly send response if paramenters not complete.
+    if(!dialogflowResponse.queryResult.allRequiredParamsPresent){
+        responseText = dialogflowResponse.queryResult.fulfillmentText;
+    }
     // Default Welcome Intent
-    if (dialogflowResponse.intent.displayName === 'Default Welcome Intent') {
+    else if (dialogflowResponse.intent.displayName === 'Default Welcome Intent') 
         responseText = await startController.welcome(dialogflowResponse, id.substring(10));
+    else if (dialogflowResponse.intent.displayName === 'Default Welcome Intent') {
+
+        //Redirects to different intents depending on number present in db
+        utils.isUser(id.substring(10),function(result){
+            if(result!=null){
+                responseText = ('Welcome back, ' +result.dataValues.first_name+' '+result.dataValues.last_name + '!\n'
+                                +'How can we help you today? Please choose from the following options:\n'
+                                +'(1) Register a new patient\n'
+                                +'(2) Check/Update existing patient\n'
+                                +'(3) Book a new consultation\n'
+                                +'(4) Follow up on existing consultation\n'
+                                +'(5) More Information about us\n');
+            }
+            else{
+                responseText = ('Welcome to E-Medic, a non-profit initiative to provide primary healthcare during COVID-19.\n'
+                                + 'How can we help you today? Please choose from the following options:\n'
+                                +'(1) Register as a user\n'
+                                +'(2) More Information about us\n');
+            }
+
+            const message = twiml.message(responseText);
+            res.send(twiml.toString());
+        });
+    }
+
+    // List of doctors intent
+    else if (dialogflowResponse.intent.displayName === 'List of doctors') {
+        const doctors = await db.doctors.findAll({}).map(
+            el => el.get('first_name') + " " + el.get('last_name')
+        );
+        responseText = responseText + "\n" + doctors.join("\n");
+
+        const message = twiml.message(responseText);
+        res.send(twiml.toString());
     }
 
     //User details Intent
@@ -70,22 +112,22 @@ router.post('/api/chat/', async function(req, res) {
         });
     }
     else if (dialogflowResponse.intent.displayName === 'register_yourself') { // Register Yourself Intent
-        responseText = userController.addPatientInfoIntent(dialogflowResponse, body);
+        responseText = userController.addPatientInfoIntent(dialogflowResponse.queryResult, body);
     }
     else if (dialogflowResponse.intent.displayName === 'check_patient_profile') { // Check single patient // Needs more work
-        responseText = userController.show(dialogflowResponse, body);
+        responseText = userController.show(dialogflowResponse.queryResult, body);
     }
     else if (dialogflowResponse.intent.displayName === 'list_of_patients') { // Complete list fo all patients
-        responseText = userController.liste(dialogflowResponse, body);
+        responseText = userController.liste(dialogflowResponse.queryResult, body);
     }
     else if (dialogflowResponse.intent.displayName === 'register_another_patient') { // Register a new Patient
-        responseText = userController.newPatientIntent(dialogflowResponse, body);
+        responseText = userController.newPatientIntent(dialogflowResponse.queryResult, body);
     }
     else if (dialogflowResponse.intent.displayName === 'user_details') { // New User Intent
-        responseText = userController.newUserIntent(dialogflowResponse, body);
+        responseText = userController.newUserIntent(dialogflowResponse.queryResult, body);
     }
     // Intents with static response handled from dialogflow console
-    else responseText = dialogflowResponse.fulfillmentText;
+    else responseText = dialogflowResponse.queryResult.fulfillmentText;
 
     const message = twiml.message(responseText);
     res.send(twiml.toString);
