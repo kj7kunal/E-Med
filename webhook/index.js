6599eb65b137e744e0acb9f48b81324cc5e2deca
Vaishnavimunghate <46276844+Vaishnavimunghate@users.noreply.github.com>
2020-06-22 12:52:59 +0530

    Patient First workflow intent mapping

--- webhook/index.js
+++ webhook/index.js
@@ -19,6 +19,7 @@ const sessionClient = new dialogflowSessionClient(projectId);
 const contextClient - new dialogflow.v2.ContextsClient();
 
 const userController = require('../controllers/UserController.js');
+const consultController = require('../controllers/ConsultController.js')
 
 router.post('/api/chat/', async function(req, res) {
     const body = req.body;
@@ -31,10 +32,30 @@ router.post('/api/chat/', async function(req, res) {
         .then(responses => {
             const cNames = responses[0];
             for (cName of cNames){
-                if (cName == "share_loc"){
+                if (cName == "share_loc" || cName == "connect_with_hospital"){
                     responseText = userController.addLocation(body);
                     contextClient.deleteContext({parent: formattedParent})
                         .catch(err => responseText += ("\n" + err));
+                }
+                if(cName == "book_consultation"){
+                    let user = await db.userWA.findOne(where: {
+                        wa_phone_number: id
+                    });
+                    db.patientWA.findAll({where: { user_id : user.id}).then(patients =>
+                      if(patients.length != 0){
+                        contextClient.deleteAllContexts({parent: formattedParent}).catch(err => {
+                          console.error(err);
+                        });
+                        const context = "check_patient";
+                        const request = {
+                          parent: formattedParent,
+                          context: context,
+                        };
+                        contextClient.createContext(request).catch(err => {
+                          console.error(err);
+                        });
+                      }
+                    )
                 }
             }
         })
@@ -126,6 +147,14 @@ router.post('/api/chat/', async function(req, res) {
     else if (dialogflowResponse.intent.displayName === 'user_details') { // New User Intent
         responseText = await userController.newUserIntent(dialogflowResponse.queryResult, body);
     }
+    // Patient First Workflow Intents
+    else if (dialogflowResponse.intent.displayName === 'book_consultation') { // Book Consultation
+        responseText = await consultController.bookConsulation(dialogflowResponse.queryResult, body);
+    }
+    else if (dialogflowResponse.intent.displayName === 'patient_info') { // List of patients for consultation
+        responseText = await consultController.patientInfo(dialogflowResponse.queryResult, body);
+    }
+
     // Intents with static response handled from dialogflow console
     else responseText = dialogflowResponse.queryResult.fulfillmentText;
 
