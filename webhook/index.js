446e25430fcaf81a5bf7421e4f81a49a405a6261
Vaishnavimunghate <46276844+Vaishnavimunghate@users.noreply.github.com>
2020-06-23 19:20:33 +0530

    Update index.js

--- webhook/index.js
+++ webhook/index.js
@@ -31,10 +31,30 @@ router.post('/api/chat/', async function(req, res) {
         .then(responses => {
             const cNames = responses[0];
             for (cName of cNames){
-                if (cName == "share_loc"){
+                if (cName == "share_loc" || "connect_with_hospital"){
                     responseText = userController.addLocation(body);
                     contextClient.deleteContext({parent: formattedParent})
                         .catch(err => responseText += ("\n" + err));
+                }
+                if(cName == "book_consultation"){
+                    let user = await db.userWA.findOne(where: {
+                        wa_phone_number: id
+                    });
+                    db.patientWA.findAll({where: { user_id : user.id}).then(patients =>
+                      if(patients.length != 0){
+                        contextClient.deleteAllContexts({parent: formattedParent}).catch(err => {
+                          console.error(err);
+                        });
+                        const context = "check_patient";
+                        const request = {
+                          parent: formattedParent,
+                          context: context,
+                        };
+                        contextClient.createContext(request).catch(err => {
+                          console.error(err);
+                        });
+                      }
+                    )
                 }
             }
         })
@@ -125,6 +145,13 @@ router.post('/api/chat/', async function(req, res) {
     }
     else if (dialogflowResponse.intent.displayName === 'user_details') { // New User Intent
         responseText = await userController.newUserIntent(dialogflowResponse.queryResult, body);
+    }
+    // Patient First Workflow Intents
+    else if (dialogflowResponse.intent.displayName === 'book_consultation') { // Book Consultation
+        responseText = await consultController.bookConsulation(dialogflowResponse.queryResult, body);
+    }
+    else if (dialogflowResponse.intent.displayName === 'patient_info') { // List of patients for consultation
+        responseText = await consultController.patientInfo(dialogflowResponse.queryResult, body);
     }
     // Intents with static response handled from dialogflow console
     else responseText = dialogflowResponse.queryResult.fulfillmentText;
