81281daae30b9262b96ed1b1f8ce784a1333a821
Kunal Jain <kunal.jain@bizreach.co.jp>
2020-07-25 20:43:32 +0900

    modified start DB

--- models/user_WA.js
+++ models/user_WA.js
@@ -15,9 +15,9 @@ module.exports = function(sequelize, DataTypes) {
                 isEmail: true
             }
         },
-        loc_lat: {
+        perm_address: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: true
         },
         loc_long: {
             type: DataTypes.STRING,
