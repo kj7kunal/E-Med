0747775ad31fa2d5c45c9974b250764e024d975c
Kunal Jain <kunal.jain@bizreach.co.jp>
2020-08-02 14:49:58 +0900

    testing 2

--- webhook/index.js
+++ webhook/index.js
@@ -47,8 +47,10 @@ router.post('/api/chat/', async function(req, res) {
         text, id, body)); // Gets intent
     const twiml = new MessagingResponse();
 
-    console.log("DF Response: " + dialogflowResponse); //remove before deploying
-    console.log("Agent parameters: " + dialogflowResponse.parameters); //remove before deploying
+    console.log("DF Response: ");
+    console.log(dialogflowResponse); //remove before deploying
+    console.log("Agent parameters: ");
+    console.log(dialogflowResponse.parameters); //remove before deploying
 
 
     /* Setting the Intent for Testing Purposes:
@@ -65,27 +67,27 @@ router.post('/api/chat/', async function(req, res) {
         responseText = await startController.welcome(dialogflowResponse, id.substring(10));
     }
     // Confirm and Create New User Intent
-    else if (dialogflowResponse.intent.displayName === 'Confirm New User') {
+    else if (dialogflowResponse.intent.displayName === 'userReg.confirm_new_user') {
         responseText = await userRegController.createNewUser(dialogflowResponse, id.substring(10));
     }
     // Show User details Intent
-    else if (dialogflowResponse.intent.displayName === 'User Profile') {
+    else if (dialogflowResponse.intent.displayName === 'userReg.show_user_profile') {
         responseText = await userRegController.showUserDetails(dialogflowResponse, id.substring(10));
     }
     // Confirm and Create New Patient Intent
-    else if (dialogflowResponse.intent.displayName === 'Confirm New Patient') {
+    else if (dialogflowResponse.intent.displayName === 'userReg.confirm_new_patient') {
         responseText = await userRegController.createNewPatient(dialogflowResponse, id.substring(10));
     }
     // Confirm and Add Patient Detailed Information Intent
-    else if (dialogflowResponse.intent.displayName === 'Confirm Patient Details') {
+    else if (dialogflowResponse.intent.displayName === 'userReg.confirm_patient_details') {
         responseText = await userRegController.addPatientInfo(dialogflowResponse, id.substring(10));
     }
     // List User Patients
-    else if (dialogflowResponse.intent.displayName === 'List of Patients') {
+    else if (dialogflowResponse.intent.displayName === 'userReg.list_of_patients') {
         responseText = await userRegController.listUserPatients(dialogflowResponse, id.substring(10));
     }
     // Check Single Patient Profile (need to add update patient prompt later)
-    else if (dialogflowResponse.intent.displayName === 'Patient Profile') {
+    else if (dialogflowResponse.intent.displayName === 'userReg.check_patient_profile') {
         responseText = await userRegController.checkPatientDetails(dialogflowResponse, id.substring(10));
     }
     // Intents with static response handled from dialogflow console
